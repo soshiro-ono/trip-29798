5c4576d18603176468a7df37e71617bff30e32d3
小野　総司郎 <ssosksono@gmail.com>
2020-10-29 01:35:06 +0900

    検索機能の実装

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -7,6 +7,20 @@ class Spot < ApplicationRecord
   # has_one_attached :ファイル名
   # Active Storageのテーブルとspotsテーブルのアソシエーションを定義した。
   # spots_controller.rbにて、imageカラムの保存を許可(ストロングパラメーター)
+
+  # バリデーション
   validates :title, presence: true
   validates :description, presence: true
+  # バリデーション
+
+  
+  # 現段階ではユーザーで検索ができない
+  def self.search(search)
+    if search != ""
+      Spot.where('title LIKE(?)', "%#{search}%")
+      Spot.where('description LIKE(?)', "%#{search}%")
+    else
+      Spot.all
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   resources :users, only: :show
   resources :spots do
     resources :comments, only: :create
+    collection do
+      get 'search'
+    end
   end
 end
