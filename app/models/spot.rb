e035067a4455303b3f7dce77a375df8c34ba5b4a
小野　総司郎 <ssosksono@gmail.com>
2020-11-07 02:07:37 +0900

    アソシエーションの設定

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,9 +1,9 @@
 class Spot < ApplicationRecord
-  has_many :spot_tags
   # has_many :tags, through: spot_tags
   has_many :comments
   belongs_to :user
   has_one_attached :image
+  belongs_to :category
   # has_one_attached :ファイル名
   # Active Storageのテーブルとspotsテーブルのアソシエーションを定義した。
   # spots_controller.rbにて、imageカラムの保存を許可(ストロングパラメーター)
