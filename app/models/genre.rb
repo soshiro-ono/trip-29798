75fd30c9590b6a156ed8eb202dcce6301310e5e7
小野　総司郎 <ssosksono@gmail.com>
2020-11-11 23:29:35 +0900

    アクティブハッシュ追加

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -6,6 +6,9 @@ class Genre < ActiveHash::Base
     { id: 4, name: '歴史' },
     { id: 5, name: 'スポーツ' },
     { id: 6, name: '博物館、美術館、記念館' },
-    { id: 7, name: 'その他' },
+    { id: 7, name: '学び' },
+    { id: 8, name: 'グルメ' },
+    { id: 9, name: '天体観測' },
+    { id: 10, name: 'その他' },
   ]
   end
--- app/models/location.rb
+++ app/models/location.rb
@@ -3,6 +3,8 @@ class Location < ActiveHash::Base
     { id: 1, name: '海' },
     { id: 2, name: '山' },
     { id: 3, name: '街' },
-    { id: 4, name: 'その他' },
+    { id: 4, name: '川' },
+    { id: 5, name: '滝' },
+    { id: 6, name: 'その他' },
   ]
   end
