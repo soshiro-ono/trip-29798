adee3e2f2efa46d00c3ceae66e54338cd41e51f0
小野　総司郎 <ssosksono@gmail.com>
2021-01-24 17:16:54 +0900

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,21 @@ ActiveRecord::Schema.define(version: 2020_10_27_090649) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "room_id"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_room_users_on_room_id"
+    t.index ["user_id"], name: "index_room_users_on_user_id"
+  end
+
+  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "spots", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "title", null: false
@@ -68,5 +83,7 @@ ActiveRecord::Schema.define(version: 2020_10_27_090649) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "room_users", "rooms"
+  add_foreign_key "room_users", "users"
   add_foreign_key "spots", "users"
 end
